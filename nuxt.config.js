5c105e20aad27bd8213846553e3588534b0b941b
Marc Lewis <marc@marclewis.io>
2020-07-25 13:33:07 +0100

    add missing envs to nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,9 @@ export default {
   },
   env: {
     CTF_SPACE_ID: process.env.CTF_SPACE_ID,
-    CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN
+    CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
+    CTF_HOMEPAGE_ID: process.env.CTF_HOMEPAGE_ID,
+    CTF_SQUAD_ID: process.env.CTF_SQUAD_ID
   },
   /*
   ** Global CSS
